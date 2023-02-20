5149b5c944fe79c04b00a39a888821c46818a47d
kausgang <write.to.kaustav@gmail.com>
2023-02-20 17:44:41 -0500

    stockfish 11 in browser

--- client/src/App.js
+++ client/src/App.js
@@ -18,8 +18,30 @@ const test_click = () => {
       // always executed
     });
 };
+
+var engine = new Worker("stockfish.js");
+
 function App() {
-  return <button onClick={test_click}>test</button>;
+  // var stockfish1 = stockfish();
+  let fen = "rnbqkbnr/pp2pppp/3p4/2pP4/2P5/8/PP2PPPP/RNBQKBNR b KQkq - 0 3";
+
+  let depth = 10;
+  engine.postMessage("position fen " + fen);
+  engine.postMessage("go depth " + depth);
+
+  engine.onmessage = function (line) {
+    // console.log(line.data);
+
+    let last_line = line.data.match("info depth " + depth);
+    let best_move = line.data.match(/bestmove\s+(\S+)/);
+    if (last_line) console.log(last_line);
+    if (best_move) console.log(best_move[1]);
+  };
+  return (
+    <>
+      <button onClick={test_click}>test</button>
+    </>
+  );
 }
 
 export default App;
