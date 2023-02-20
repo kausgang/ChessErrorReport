ab222a1b3f6916c9d1cf27267e69ce8b93dbb248
kausgang <write.to.kaustav@gmail.com>
2023-02-19 23:11:59 -0500

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
+// let arr = [];
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   // res.render('index', { title: 'Express' });
@@ -8,14 +10,20 @@ router.get("/", function (req, res, next) {
   let engine = req.app.locals.engine;
   let fen = "rnbqkbnr/pp2pppp/3p4/2pP4/2P5/8/PP2PPPP/RNBQKBNR b KQkq - 0 3";
 
+  let depth = 10;
   engine.postMessage("position fen " + fen);
-  engine.postMessage("go depth 5");
+  engine.postMessage("go depth " + depth);
 
   engine.onmessage = function (line) {
     console.log(line);
-  };
 
-  res.status(200).send({ a: "A" });
+    let last_line = line.match("info depth " + depth);
+    let best_move = line.match(/bestmove\s+(\S+)/);
+    if (last_line) console.log(last_line);
+    if (best_move) console.log(best_move[1]);
+
+    // res.status(200).send({});
+  };
 });
 
 module.exports = router;
