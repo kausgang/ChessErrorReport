3adeb5fa7cd2aeec5773aa3e16e2d161f8c70bc4
Kaustav Ganguli <33816465+kausgang@users.noreply.github.com>
2023-03-06 15:16:46 -0500

    chess python

--- server/app.py
+++ server/app.py
@@ -1,16 +1,29 @@
 from flask import Flask, request
 from flask_cors import CORS
+from stockfish import Stockfish
+import chess
+import chess.pgn
 
 app = Flask(__name__)
 
 CORS(app)
 
-
-
-
-
+stockfish = Stockfish(path="./STOCKFISH/stockfish-windows-2022-x86-64-avx2.exe")
 
 @app.post("/analyze")
 def analyze_game():
-    print(request.get_json())
+    pgn_json=request.get_json()
+    pgn=pgn_json['pgn']
+
+    
+    game = chess.pgn.read_game(pgn)
+    print(game)
+    pgn_board = game.board()
+
+    board=chess.Board()
+
+    # for move in game.mainline_moves():
+    #     print(move)
+
+    # print(board.legal_moves )
     return "got pgn" , 200
